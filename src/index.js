efb9a16fb41261d4955837378d8b1258f0f16d4e
Adwoa-Safowaah <adwoaadysne@gmail.com>
2024-11-27 12:18:06 +0000

    Made changes in my folders and files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//alert("Hi there");
+console.log(33);
+let num2 = 2
+let num3 = 4
+num2%num3
+console.log(num2);
+console.log(num3);
+console.log(num2%num3);
+let result = (num2 +num3);
+console.log(result);
+let job = ("mining");
+console.log(`My dream job is ${job}`);
